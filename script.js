b9b9039aa7f643c7c31897fe6faf7b38abf7c0cf
Nikhil Rawat <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-21 12:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function myfunc(event) {
 	const text = event.target.value;
 	const len = text.length;
-	console.log(text);
 	document.getElementById("letterCount").innerText = len;
 }
 const input = document.getElementById("evaluatedText");
